7e64cab300371d1bf553a425ccc5ae0a3962aa5e
kszwarc <konrad.schwartz@gmail.com>
2019-05-24 19:44:32 +0200

    No need for being verbose.

--- common/db_connector.py
+++ common/db_connector.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(os.environ["DB_URL"], echo=True)
+engine = create_engine(os.environ["DB_URL"])
 
 Session = sessionmaker(bind=engine)
 
